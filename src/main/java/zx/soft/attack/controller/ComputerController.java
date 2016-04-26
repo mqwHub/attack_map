f929889784bef6a546b5bfca022feaa813acf5eb
caihanling <caihanling@zxisl.com>
2016-04-26 17:27:23 +0800

    测试框架

--- src/main/java/zx/soft/attack/controller/ComputerController.java
+++ src/main/java/zx/soft/attack/controller/ComputerController.java
@@ -1,12 +1,19 @@
 package zx.soft.attack.controller;
 
 
+import java.util.List;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import zx.soft.attack.model.Computer;
 import zx.soft.attack.service.ComputerService;
@@ -19,8 +26,11 @@ public class ComputerController {
 	private ComputerService computerService;
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String getComputer() {
-		return "index";
+	@ResponseStatus(HttpStatus.OK)
+	public String getComputer(HttpServletRequest request, HttpServletResponse response, Model model) {	
+		List list = computerService.getComputerData();
+		model.addAllAttributes(list);
+		return "redirect:/WEB-INF/views/index.jsp";
 	}
 
 
